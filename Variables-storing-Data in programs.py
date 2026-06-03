34bde036a34fdf94292fc25e150a6a83170fec5b
Makhadzi Selolo <Makhadziselolo2604@outlook.com>
2026-06-03 02:25:05 +0200

    Add files via upload

---
+++ Variables-storing-Data in programs.py
@@ -0,0 +1,9 @@
+name = "John Doe"
+fact = "A traffic jam lasted lasted for more than 10 days, with cars only moving 0.6 miles a day."
+address = "77 Winchester Lane"
+empty_str = ""
+
+print(name)
+print(fact)
+print(address)
+print(empty_str)
